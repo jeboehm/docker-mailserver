a38028c33cd6d0300e37528ba0b27a6c2ba10425
Jeffrey Boehm <jeff@ressourcenkonflikt.de>
2016-05-29 22:59:53 +0200

    Added database bootstrap files

---
+++ db/dumps/00-init.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS mailserver;
+USE mailserver;
+
+CREATE TABLE IF NOT EXISTS `virtual_domains` (
+ `id` int(11) NOT NULL auto_increment,
+ `name` varchar(50) NOT NULL,
+ PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `virtual_users` (
+ `id` int(11) NOT NULL auto_increment,
+ `domain_id` int(11) NOT NULL,
+ `email` varchar(100) NOT NULL,
+ `password` varchar(150) NOT NULL,
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `email` (`email`),
+ FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `virtual_aliases` (
+ `id` int(11) NOT NULL auto_increment,
+ `domain_id` int(11) NOT NULL,
+ `source` varchar(100) NOT NULL,
+ `destination` varchar(100) NOT NULL,
+ PRIMARY KEY (`id`),
+ FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
