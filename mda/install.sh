9369eff9f31c8a31f91805d42f872b773a88e2da
Jeffrey Boehm <jeff@ressourcenkonflikt.de>
2016-05-29 20:00:15 +0200

    Initial dovecot configuration

---
+++ mda/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /patch
+for patch in $(ls -1)
+do
+	sed -i ${patch} \
+		-e "s/#dbname#/${MYSQL_DBNAME}/g" \
+		-e "s/#dbhost#/${MYSQL_HOST}/g" \
+		-e "s/#dbpassword#/${MYSQL_PASSWORD}/g" \
+		-e "s/#dbuser#/${MYSQL_USER}/g"
+
+	patch -p0 < $patch
+done
+
+chown root:root /etc/dovecot/dovecot-sql.conf.ext
+chmod go= /etc/dovecot/dovecot-sql.conf.ext
