c559b7d90cc6886c93435dfcaa91aaa24a035f9e
Jeffrey Boehm <jeff@ressourcenkonflikt.de>
2016-06-01 08:31:23 +0200

    create lockfile, to create the database only once

--- db/init.sh
+++ db/init.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-sed -i /scripts/pre-exec.d/init.sql -e "s/##db##/${MYSQL_DATABASE}/g"
-/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < /scripts/pre-exec.d/init.sql
+LOCKFILE="/var/lib/mysql/init.lock"
+
+if ! [ -r $LOCKFILE ]
+then
+	sed -i /scripts/pre-exec.d/init.sql -e "s/##db##/${MYSQL_DATABASE}/g"
+	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < /scripts/pre-exec.d/init.sql
+	touch $LOCKFILE
+fi
