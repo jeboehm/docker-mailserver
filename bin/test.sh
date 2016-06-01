7d7acb83673d04f656b1d5475a37205686221fda
Jeffrey Boehm <jeff@ressourcenkonflikt.de>
2016-06-01 23:43:52 +0200

    Seperate environments

---
+++ bin/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+## Guard against empty $DIR
+if [[ "$DIR" != */bin ]]; then
+    echo "Could not detect working directory."
+    exit 1
+fi
+
+cd $DIR/../
+
+
+
+docker-compose \
+  -f docker-compose.yml \
+  -f docker-compose.public.yml \
+  -f docker-compose.manager.yml \
+  -f docker-compose.manager-public.yml \
+  $*
