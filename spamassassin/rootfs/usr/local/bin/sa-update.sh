34f62aea29dc252f625039f0612717c87d71f411
Jeffrey Boehm <j.boehm@ressourcenkonflikt.de>
2016-09-25 19:11:05 +0200

    Feature/spamassassin (#9)
    * Lesser spamassassin restarts, moved gpg key dir to $HOME
    
    * Use razor2 and pyzor
    
    * No need to install docker anymore

--- spamassassin/rootfs/usr/local/bin/sa-update.sh
+++ spamassassin/rootfs/usr/local/bin/sa-update.sh
@@ -1,18 +1,48 @@
 #!/bin/sh
 
+PIDFILE="/run/spamd.pid"
+
+do_update() {
+  start-stop-daemon \
+    --chuid debian-spamd:debian-spamd --start \
+    --exec /usr/bin/sa-update -- \
+    --gpghomedir /var/lib/spamassassin/sa-update-keys 2>&1
+}
+
+do_reload() {
+  kill -1 $(cat $PIDFILE)
+}
+
+if [ "$1" = "--startup" ]
+then
+  do_update
+  exit 0
+fi
+
 sleep 60
 
 while true
 do
-    if ! [ -r /run/spamd.pid ]
-    then
-        "spamd not running. Exit."
-        exit 1
-    fi
+  if ! [ -r $PIDFILE ]
+  then
+      "spamd not running. Exit."
+      exit 1
+  fi
 
-	echo "Updating spamassassin ruleset.."
-	sa-update
-	kill -1 $(cat /var/run/spamd.pid)
+  do_update
+
+  case $? in
+    0)
+      echo "Rule updates installed."
+      do_reload
+      ;;
+    1)
+      echo "No rule updates found."
+      ;;
+    *)
+      echo "sa-update failed for unknown reasons."
+      ;;
+  esac
 
 	sleep 6h
 done
--- spamassassin/rootfs/usr/local/bin/spamd.sh
+++ spamassassin/rootfs/usr/local/bin/spamd.sh
@@ -1,16 +1,42 @@
 #!/bin/sh
-
+SA_HOME="/var/lib/spamassassin/.spamassassin"
+PYZOR_HOME="/var/lib/spamassassin/.pyzor"
+RAZOR_HOME="/var/lib/spamassassin/.razor"
 echo "Starting spamassassin.."
 
-sa-update
+create_homedir() {
+  if ! [ -d ${SA_HOME} ]
+  then
+      mkdir ${SA_HOME}
+      chown debian-spamd ${SA_HOME}
+  fi
+}
 
-SA_HOME=/var/lib/spamassassin/.spamassassin
+pyzor_discover() {
+  start-stop-daemon \
+    --chuid debian-spamd:debian-spamd --start \
+    --exec /usr/bin/pyzor -- \
+    --homedir ${PYZOR_HOME} discover
+}
 
-if ! [ -d ${SA_HOME} ]
-then
-    mkdir ${SA_HOME}
-    chown debian-spamd ${SA_HOME}
-fi
+razor_setup() {
+  if [ -d $RAZOR_HOME ]
+  then
+    return 0
+  fi
+
+  mkdir $RAZOR_HOME
+
+  razor-admin -home=$RAZOR_HOME -register
+  razor-admin -home=$RAZOR_HOME -create
+  razor-admin -home=$RAZOR_HOME -discover
+}
+
+create_homedir
+pyzor_discover
+razor_setup
+
+/usr/local/bin/sa-update.sh --startup
 
 spamd \
     --username=debian-spamd \
