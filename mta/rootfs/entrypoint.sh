233e37d1638c5f5bbc9b4afb5194c6fb4c075b8a
Jeffrey Böhm <jeffrey.boehm@twt.de>
2016-10-10 22:10:31 +0200

    Added greylisting function

--- mta/rootfs/entrypoint.sh
+++ mta/rootfs/entrypoint.sh
@@ -14,8 +14,14 @@ do
         "${file}" > "/etc/postfix/`basename ${file}`"
 done
 
-echo "Starting MTA..."
+rm -f /etc/supervisor.d/postgrey.ini
+postconf -X smtpd_recipient_restrictions
 
+if [ $GREYLISTING_ENABLED == "true" ]
+then
+  cp /config/supervisord/postgrey.ini /etc/supervisor.d/postgrey.ini
+  postconf smtpd_recipient_restrictions="permit_mynetworks permit_sasl_authenticated reject_unauth_destination check_policy_service inet:127.0.0.1:10023"
+fi
 
 newaliases
 /usr/bin/supervisord
