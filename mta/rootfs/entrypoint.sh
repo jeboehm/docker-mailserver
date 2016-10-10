0359e173b41728cd0335c5a0c73ebb69961dbd9b
Jeffrey Böhm <jeffrey.boehm@twt.de>
2016-10-11 00:37:33 +0200

    Simplify init process

--- mta/rootfs/entrypoint.sh
+++ mta/rootfs/entrypoint.sh
@@ -3,24 +3,22 @@
 postconf myhostname="${MAILNAME}"
 postconf mynetworks="${MYNETWORKS}"
 
-for file in /config/postfix/*.cf
+for file in /etc/postfix/mysql-*.cf
 do
     if ! [ -e "${file}" ]; then break; fi
-    sed \
+    sed -i \
         -e "s/#dbname#/${MYSQL_DATABASE}/g" \
         -e "s/#hosts#/${MYSQL_HOST}/g" \
         -e "s/#password#/${MYSQL_PASSWORD}/g" \
         -e "s/#user#/${MYSQL_USER}/g" \
-        "${file}" > "/etc/postfix/`basename ${file}`"
+        "${file}"
 done
 
-rm -f /etc/supervisor.d/postgrey.ini
-postconf -X smtpd_recipient_restrictions
-
-if [ $GREYLISTING_ENABLED == "true" ]
+if [ ${GREYLISTING_ENABLED} == "true" ]
 then
-  cp /config/supervisord/postgrey.ini /etc/supervisor.d/postgrey.ini
   postconf smtpd_recipient_restrictions="permit_mynetworks permit_sasl_authenticated reject_unauth_destination check_policy_service inet:127.0.0.1:10023"
+else
+  rm /etc/supervisor.d/postgrey.ini
 fi
 
 newaliases
