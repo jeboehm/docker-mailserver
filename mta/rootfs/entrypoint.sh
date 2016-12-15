5dc9a779aed77571bbdbf6ce172efa369697c735
Jeffrey Böhm <jeffrey.boehm@twt.de>
2016-12-15 11:50:33 +0100

    Added mime filter function

--- mta/rootfs/entrypoint.sh
+++ mta/rootfs/entrypoint.sh
@@ -18,4 +18,9 @@ then
   postconf smtpd_recipient_restrictions="permit_mynetworks permit_sasl_authenticated reject_unauth_destination check_policy_service inet:127.0.0.1:10023"
 fi
 
+if [ "${FILTER_MIME}" == "true" ]
+then
+  postconf mime_header_checks = regexp:/etc/postfix/mime_header_checks
+fi
+
 /usr/bin/supervisord
