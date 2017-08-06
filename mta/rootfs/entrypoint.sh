dec1cbee378587c613011a63bbc53c7ce8759309
Jeffrey Böhm <jeffrey.boehm@twt.de>
2017-08-06 17:35:53 +0200

    MTA: use dockerize for configuration

--- mta/rootfs/entrypoint.sh
+++ mta/rootfs/entrypoint.sh
@@ -2,17 +2,6 @@
 postconf myhostname="${MAILNAME}"
 postconf mynetworks="${MYNETWORKS}"
 
-for file in /etc/postfix/mysql-*.cf
-do
-    if ! [ -e "${file}" ]; then break; fi
-    sed -i \
-        -e "s/#dbname#/${MYSQL_DATABASE}/g" \
-        -e "s/#hosts#/${MYSQL_HOST}/g" \
-        -e "s/#password#/${MYSQL_PASSWORD}/g" \
-        -e "s/#user#/${MYSQL_USER}/g" \
-        "${file}"
-done
-
 if [ "${GREYLISTING_ENABLED}" == "true" ]
 then
   postconf smtpd_recipient_restrictions="permit_mynetworks permit_sasl_authenticated reject_unauth_destination check_policy_service inet:127.0.0.1:10023"
@@ -23,4 +12,12 @@ then
   postconf mime_header_checks=regexp:/etc/postfix/mime_header_checks
 fi
 
-/usr/bin/supervisord
+dockerize \
+  -template /etc/postfix/mysql-email2email.cf.templ:/etc/postfix/mysql-email2email.cf \
+  -template /etc/postfix/mysql-virtual-alias-maps.cf.templ:/etc/postfix/mysql-virtual-alias-maps.cf \
+  -template /etc/postfix/mysql-virtual-mailbox-domains.cf.templ:/etc/postfix/mysql-virtual-mailbox-domains.cf \
+  -template /etc/postfix/mysql-virtual-mailbox-maps.cf.templ:/etc/postfix/mysql-virtual-mailbox-maps.cf \
+  -wait tcp://${MYSQL_HOST}:3306 \
+  -wait tcp://${MDA_HOST}:2003 \
+  -wait tcp://${SPAMASSASSIN_HOST}:9999 \
+  /usr/bin/supervisord
