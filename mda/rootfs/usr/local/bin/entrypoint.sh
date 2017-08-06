d8119b8ece944a8dd4b3f7da434c3328977ba0b5
Jeffrey Böhm <jeffrey.boehm@twt.de>
2017-08-06 16:55:17 +0200

    MDA: IMAP and POP3 can be disabled now

--- mda/rootfs/usr/local/bin/entrypoint.sh
+++ mda/rootfs/usr/local/bin/entrypoint.sh
@@ -5,6 +5,7 @@ then
 fi
 
 dockerize \
+  -template /etc/dovecot/conf.d/10-master.conf.templ:/etc/dovecot/conf.d/10-master.conf \
   -template /etc/dovecot/conf.d/15-lda.conf.templ:/etc/dovecot/conf.d/15-lda.conf \
   -template /etc/dovecot/dovecot-sql.conf.ext.templ:/etc/dovecot/dovecot-sql.conf.ext \
   -wait tcp://${MYSQL_HOST}:3306 \
