5a48a2279f656e20fb1497a3d5a4fcad8b9e8214
Jeff Boehm <jeffrey.boehm@surplex.com>
2018-04-23 12:57:56 +0200

    Wait until ClamAV is available

--- filter/rootfs/usr/local/bin/entrypoint.sh
+++ filter/rootfs/usr/local/bin/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+FILTER_VIRUS_ARGS=""
+if [ ${FILTER_VIRUS} == "true" ]
+then
+    FILTER_VIRUS_ARGS="-wait tcp://${FILTER_VIRUS_HOST}:3310"
+fi
+
 dockerize \
   -template /etc/rspamd/override.d/antivirus.conf.templ:/etc/rspamd/override.d/antivirus.conf \
+  ${FILTER_VIRUS_ARGS} \
   /usr/sbin/rspamd -c /etc/rspamd/rspamd.conf -f
