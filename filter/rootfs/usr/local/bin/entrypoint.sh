101c8afb5bf7dae43787faaed9b384739a4c7919
Jeff Boehm <jeffrey.boehm@surplex.com>
2018-05-14 19:00:52 +0200

    Allow setting the web controller password

--- filter/rootfs/usr/local/bin/entrypoint.sh
+++ filter/rootfs/usr/local/bin/entrypoint.sh
@@ -16,8 +16,16 @@ then
     cp /usr/share/rspamd/bayes.ham.sqlite /var/lib/rspamd/bayes.ham.sqlite
 fi
 
+if [ "${CONTROLLER_PASSWORD}" == "changeme" ]
+then
+    CONTROLLER_PASSWORD="q1"
+fi
+
+export CONTROLLER_PASSWORD_ENC=`rspamadm pw -e -p ${CONTROLLER_PASSWORD}`
+
 dockerize \
   -template /etc/rspamd/local.d/antivirus.conf.templ:/etc/rspamd/local.d/antivirus.conf \
+  -template /etc/rspamd/local.d/worker-controller.inc.templ:/etc/rspamd/local.d/worker-controller.inc \
   ${FILTER_VIRUS_ARGS} \
   -timeout ${WAITSTART_TIMEOUT} \
   /usr/sbin/rspamd -c /etc/rspamd/rspamd.conf -f
