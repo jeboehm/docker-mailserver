f417cc9d627c86c5078a922a3a92af6217ea58a4
Jeff Boehm <jeffrey.boehm@surplex.com>
2018-05-22 09:18:12 +0200

    Simplify web init process, add helper script for creating fixtures, add fixtures target in Makefile

--- web/rootfs/usr/local/bin/entrypoint.sh
+++ web/rootfs/usr/local/bin/entrypoint.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 set -e
 
+manager_init() {
+  cd /opt/manager
+
+  bin/console doctrine:migrations:migrate -n
+  bin/console doctrine:schema:update --force
+}
+
+roundcube_init() {
+  cd /var/www/html/webmail
+  PWD=`pwd`
+
+  bin/initdb.sh --dir=$PWD/SQL || bin/updatedb.sh --dir=$PWD/SQL --package=roundcube || echo "Failed to initialize databse. Please run $PWD/bin/initdb.sh manually."
+}
+
 dockerize \
   -wait tcp://${MYSQL_HOST}:3306 \
-  -timeout ${WAITSTART_TIMEOUT} \
-  /usr/local/bin/rc_init.sh
-
-/usr/local/bin/manager_init.sh
-
-dockerize \
   -wait tcp://${MDA_HOST}:143 \
   -wait tcp://${MTA_HOST}:25 \
   -wait tcp://${FILTER_HOST}:11334 \
-  -timeout ${WAITSTART_TIMEOUT} \
-  /usr/bin/supervisord
+  -timeout ${WAITSTART_TIMEOUT}
+
+manager_init
+roundcube_init
+
+/usr/bin/supervisord
