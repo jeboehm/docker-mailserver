51e64c892361ea7ac04853d03271f2721cd7b848
Jeff Boehm <jeffrey.boehm@surplex.com>
2018-07-01 15:43:06 +0200

    Lint Dockerfiles

---
+++ .ci/bin/dockerfile_lint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+IGNORE_RULES="DL3018"
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+## Guard against empty $DIR
+if [[ "$DIR" != */bin ]]; then
+    echo "Could not detect working directory."
+    exit 1
+fi
+
+cd $DIR/../../
+
+FILES=$(find . -type f -name Dockerfile)
+HASERRORS=false
+IGNORE=""
+
+for ignore_rule in $IGNORE_RULES
+do
+    IGNORE="$IGNORE --ignore $ignore_rule"
+done
+
+for file in $FILES
+do
+    echo "Running hadolint for $file..."
+    docker run --rm -i hadolint/hadolint hadolint $IGNORE - < $file
+
+    if [ $? != 0 ]
+    then
+        HASERRORS=true
+    fi
+
+    echo "============================="
+done
+
+if [ $HASERRORS = "true" ]
+then
+    echo "Found errors."
+    exit 1
+fi
+
+echo "No errors found."
+exit 0
