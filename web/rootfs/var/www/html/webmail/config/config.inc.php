186e50b6f41cd5ec37fdc6fedfdebc4e17291d5f
Jeff Boehm <jeffrey.boehm@surplex.com>
2019-05-11 22:17:16 +0200

    Change smtp to submission port

--- web/rootfs/var/www/html/webmail/config/config.inc.php
+++ web/rootfs/var/www/html/webmail/config/config.inc.php
@@ -13,8 +13,8 @@ $config['db_dsnw'] = sprintf(
     getenv('MYSQL_DATABASE')
 );
 $config['default_host'] = 'tls://' . getenv('MDA_HOST');
-$config['smtp_server'] = 'tls://' . getenv('MTA_HOST');
-$config['smtp_port'] = 25;
+$config['smtp_server'] = 'tls://' . getenv('MDA_HOST');
+$config['smtp_port'] = 587;
 $config['smtp_user'] = '%u';
 $config['smtp_pass'] = '%p';
 $config['support_url'] = getenv('SUPPORT_URL');
