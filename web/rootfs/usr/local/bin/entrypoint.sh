c61826ff8603a643bbdd26c6b09b89b1343040b2
Jeff Boehm <jeff@ressourcenkonflikt.de>
2019-09-01 16:35:48 +0200

    Prepare DKIM signing

--- web/rootfs/usr/local/bin/entrypoint.sh
+++ web/rootfs/usr/local/bin/entrypoint.sh
@@ -15,14 +15,28 @@ roundcube_init() {
   bin/initdb.sh --dir=$PWD/SQL || bin/updatedb.sh --dir=$PWD/SQL --package=roundcube || echo "Failed to initialize databse. Please run $PWD/bin/initdb.sh manually."
 }
 
+permissions() {
+  chown -R www-data /media/dkim
+  chmod 777 /media/dkim
+}
+
+dkim_refresh() {
+  cd /opt/manager
+
+  bin/console dkim:refresh
+}
+
 dockerize \
   -wait tcp://${MYSQL_HOST}:3306 \
   -wait tcp://${MDA_HOST}:143 \
   -wait tcp://${MTA_HOST}:25 \
   -wait tcp://${FILTER_HOST}:11334 \
+  -wait file:///media/dkim/ \
   -timeout ${WAITSTART_TIMEOUT}
 
 manager_init
 roundcube_init
+permissions
+dkim_refresh
 
 /usr/bin/supervisord
