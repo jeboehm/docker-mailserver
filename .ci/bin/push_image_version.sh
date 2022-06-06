32d8a5aecb8bb66fe89ccfe09aa37768da22e1e5
Jeffrey Böhm <hello@jeffrey-boehm.de>
2022-06-06 13:16:37 +0200

    chore(ci): Publish images based on the next branch

--- .ci/bin/push_image_version.sh
+++ .ci/bin/push_image_version.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
+VERSION="${1}"
 COMPONENTS="mda mta db filter ssl virus web"
+
+if [ "${VERSION}" == "" ]
+then
+    echo "Expected version string!"
+
+    exit 1
+fi
 
 for component in $COMPONENTS
 do
-    docker tag jeboehm/mailserver-$component:latest jeboehm/mailserver-$component:$1
+    docker tag jeboehm/mailserver-$component:latest jeboehm/mailserver-$component:${VERSION}
     
-    docker push jeboehm/mailserver-$component:latest
-    docker push jeboehm/mailserver-$component:$1
+    if [ "${VERSION}" != "next" ]
+    then
+        docker push jeboehm/mailserver-$component:latest
+    fi
+    
+    docker push jeboehm/mailserver-$component:${VERSION}
 done
