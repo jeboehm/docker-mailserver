82c1cd9c5753a65dcb0f4d2f38e1c3939e703360
Jeffrey Böhm <hello@jeffrey-boehm.de>
2023-03-10 15:02:48 +0100

    fix(web): fixed managesieve login issues

--- web/rootfs/var/www/html/webmail/config/config.inc.php
+++ web/rootfs/var/www/html/webmail/config/config.inc.php
@@ -28,13 +28,13 @@ $config['imap_conn_options'] = [
     'ssl' => [
         'verify_peer'       => false,
         'verify_peer_name'  => false,
-        'allow_self_signed' => false,
+        'allow_self_signed' => true,
     ],
 ];
 $config['smtp_conn_options'] = [
     'ssl' => [
         'verify_peer'       => false,
         'verify_peer_name'  => false,
-        'allow_self_signed' => false,
+        'allow_self_signed' => true,
     ],
 ];
