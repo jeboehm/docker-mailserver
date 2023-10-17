afe4ac8301b18e66c90c0688b279c8165a9085fa
Jeffrey Böhm <hello@jeffrey-boehm.de>
2023-10-17 20:18:23 +0200

    feat: use "docker compose" in wrapper scripts when available (closes #232)

--- bin/production.sh
+++ bin/production.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+# Helper script for docker compose.
+# Usage: bin/test.sh [COMMAND]
+# Example: bin/test.sh up -d
+#
+# You can also use docker compose directly, but this script
+# will make sure that the correct compose files are used.
+
+set -e
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
+BIN="docker comspose"
 
 ## Guard against empty $DIR
 if [[ "$DIR" != */bin ]]; then
     echo "Could not detect working directory."
     exit 1
 fi
+
+$BIN version --short >/dev/null 2>&1 || {
+    BIN=$(which docker-compose)
+}
 
 cd "${DIR}/../" || exit
 
@@ -17,7 +30,7 @@ else
     ADDITIONAL=""
 fi
 
-docker-compose \
+$BIN \
   -f docker-compose.yml \
   -f docker-compose.production.yml \
   ${ADDITIONAL} \
