e3ed3f5c55a8e9de324d661d961ada063f58c69a
Jeffrey Böhm <hello@jeffrey-boehm.de>
2024-01-30 12:03:28 +0100

    feat(filter): add option to override rspamd dns servers #273

--- filter/rootfs/usr/local/bin/entrypoint.sh
+++ filter/rootfs/usr/local/bin/entrypoint.sh
@@ -19,6 +19,7 @@ export CONTROLLER_PASSWORD_ENC
 # shellcheck disable=SC2086
 dockerize \
   -template /etc/rspamd/local.d/antivirus.conf.templ:/etc/rspamd/local.d/antivirus.conf \
+  -template /etc/rspamd/local.d/options.inc.templ:/etc/rspamd/local.d/options.inc \
   -template /etc/rspamd/local.d/worker-controller.inc.templ:/etc/rspamd/local.d/worker-controller.inc \
   -template /etc/rspamd/override.d/redis.conf.templ:/etc/rspamd/override.d/redis.conf \
   -template /etc/rspamd/local.d/classifier-bayes.conf.templ:/etc/rspamd/local.d/classifier-bayes.conf \
