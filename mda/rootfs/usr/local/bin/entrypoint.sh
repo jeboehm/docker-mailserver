0136bc470e4e37cd1ff6abd0c1a595221afd3c34
Jeffrey Boehm <3028277+jeboehm@users.noreply.github.com>
2025-02-19 22:58:24 +0100

    chore: split init scripts for easier container start up (#359)

--- mda/rootfs/usr/local/bin/entrypoint.sh
+++ mda/rootfs/usr/local/bin/entrypoint.sh
@@ -6,16 +6,15 @@ then
   /usr/local/bin/dh.sh &
 fi
 
+if [ "${SKIP_INIT}" != "true" ]; then
+    /usr/local/bin/init.sh
+fi
+
 rm -f /run/dovecot/master.pid
 
 dockerize \
-  -template /etc/dovecot/conf.d/10-master.conf.templ:/etc/dovecot/conf.d/10-master.conf \
-  -template /etc/dovecot/conf.d/15-lda.conf.templ:/etc/dovecot/conf.d/15-lda.conf \
-  -template /etc/dovecot/conf.d/15-fts-xapian.conf.templ:/etc/dovecot/conf.d/15-fts-xapian.conf \
-  -template /etc/dovecot/conf.d/90-sieve.conf.templ:/etc/dovecot/conf.d/90-sieve.conf \
-  -template /etc/dovecot/dovecot-sql.conf.ext.templ:/etc/dovecot/dovecot-sql.conf.ext \
-  -wait tcp://${MYSQL_HOST}:${MYSQL_PORT} \
-  -wait file://${SSL_CERT} \
-  -wait file://${SSL_KEY} \
-  -timeout ${WAITSTART_TIMEOUT} \
+  -wait "tcp://${MYSQL_HOST}:${MYSQL_PORT}" \
+  -wait "file://${SSL_CERT}" \
+  -wait "file://${SSL_KEY}" \
+  -timeout "${WAITSTART_TIMEOUT}" \
   /usr/sbin/dovecot -F
---
+++ web/rootfs/usr/local/bin/init_database.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This script is used to initialize the database.
+set -e
+
+roundcube_init() {
+  cd /var/www/html/webmail
+  PWD=$(pwd)
+
+  bin/initdb.sh --dir="$PWD/SQL" || bin/updatedb.sh --dir="$PWD/SQL" --package=roundcube || echo "Failed to initialize databse. Please run $PWD/bin/initdb.sh manually."
+  rm -f /var/www/html/webmail/logs/errors.log
+}
+
+roundcube_init
+
+/opt/manager/bin/console doctrine:migrations:migrate -n
+/opt/manager/bin/console dkim:refresh
