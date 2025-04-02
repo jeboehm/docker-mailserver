ddbc35a972c8dbda22d57332a47d309517375f68
Jeffrey Böhm <hello@jeffrey-boehm.de>
2025-04-02 11:02:58 +0200

    feat(web): allow to run web container as non-root

--- test/rootfs/usr/local/bin/run-tests.sh
+++ test/rootfs/usr/local/bin/run-tests.sh
@@ -3,7 +3,7 @@
 dockerize \
 	-wait tcp://db:3306 \
 	-wait tcp://mta:25 \
-	-wait tcp://web:80 \
+	-wait tcp://web:8080 \
 	-wait tcp://mda:143 \
 	-wait tcp://filter:11334 \
 	-timeout "${WAITSTART_TIMEOUT}" \
--- web/rootfs/usr/local/bin/fixtures.sh
+++ web/rootfs/usr/local/bin/fixtures.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dockerize \
-	-wait "tcp://${WEB_HOST}:80" \
+	-wait "tcp://${WEB_HOST}:8080" \
 	-wait "tcp://${MYSQL_HOST}:${MYSQL_PORT}" \
 	-timeout "${WAITSTART_TIMEOUT}" \
 	"${@}"
