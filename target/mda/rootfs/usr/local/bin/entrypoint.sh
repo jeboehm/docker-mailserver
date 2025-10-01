48137af8bb25b2f8c4ccca7c21d0a933a7688fa0
Jeffrey Böhm <hello@jeffrey-boehm.de>
2025-10-01 22:22:34 +0200

    fix bool variable

--- target/mda/rootfs/usr/local/bin/entrypoint.sh
+++ target/mda/rootfs/usr/local/bin/entrypoint.sh
@@ -5,6 +5,16 @@ if [ -r /run/dovecot/master.pid ]; then
 	rm /run/dovecot/master.pid
 fi
 
+if [ -n "${MDA_UPSTREAM_PROXY}" ]; then
+	if [ "${MDA_UPSTREAM_PROXY}" = "true" ]; then
+		export MDA_UPSTREAM_PROXY="yes"
+	elif [ "${MDA_UPSTREAM_PROXY}" = "false" ]; then
+		export MDA_UPSTREAM_PROXY="no"
+	else
+		export MDA_UPSTREAM_PROXY
+	fi
+fi
+
 dockerize \
 	-wait "tcp://${MYSQL_HOST}:${MYSQL_PORT}" \
 	-wait "file:///etc/dovecot/tls/tls.crt" \
