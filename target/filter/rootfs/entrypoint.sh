7d86efd917ceca5839c9dc269276b660562b9abe
Jeffrey Böhm <hello@jeffrey-boehm.de>
2025-12-12 18:10:19 +0100

    feat: use entrypoint scripts instead of CMDs, display a banner to identify the container

--- target/filter/rootfs/entrypoint.sh
+++ target/filter/rootfs/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 if [ "${CONTROLLER_PASSWORD}" = "" ]; then
 	# q1 is disabled in rspamd.
@@ -12,5 +13,11 @@ export RSPAMD_CONTROLLER_PASSWORD_ENC
 export RSPAMD_REDIS_PASSWORD="${REDIS_PASSWORD}"
 export RSPAMD_REDIS_HOST="${REDIS_HOST}"
 export RSPAMD_REDIS_PORT="${REDIS_PORT}"
+
+[ "$#" -gt 0 ] && exec "$@"
+
+if [ -r /.banner.sh ]; then
+	/.banner.sh
+fi
 
 exec /usr/bin/rspamd -f
