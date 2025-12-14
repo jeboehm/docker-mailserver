8f4d6d90ed622c0a901c995bc6a90c53f60d7dc1
Jeffrey Böhm <hello@jeffrey-boehm.de>
2025-12-14 23:01:27 +0100

    feat(web): update mailserver-admin to 5.9.0

--- target/web/rootfs/usr/local/lib/init.sh
+++ target/web/rootfs/usr/local/lib/init.sh
@@ -2,8 +2,4 @@
 # This script is used to initialize the container.
 set -e
 
-dockerize \
-	-template "/opt/autoconfig/config-v1.1.xml.templ:${SERVER_ROOT}/autoconfig/config-v1.1.xml" \
-	/bin/true
-
 /usr/local/lib/init_database.sh
