e6d97d11b47c8bcf1a5871e30d94c1d589ece7bf
Jeffrey Böhm <hello@jeffrey-boehm.de>
2026-01-31 11:43:39 +0100

    test: replace dockerize with wait-for-services script

--- test/bats/rootfs/entrypoint.sh
+++ test/bats/rootfs/entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
 if [ "${IS_KUBERNETES}" -eq "1" ]; then
-	/usr/local/bin/kubectl.sh
+	/usr/local/lib/kubectl.sh
 fi
 
-exec dockerize \
-	-wait "tcp://${MYSQL_HOST}:${MYSQL_PORT}" \
-	-wait "tcp://${FILTER_WEB_ADDRESS}" \
-	-wait "tcp://${MDA_IMAP_ADDRESS}" \
-	-wait "tcp://${MTA_SMTP_SUBMISSION_ADDRESS}" \
-	-wait "tcp://${WEB_HTTP_ADDRESS}" \
-	-timeout "${WAITSTART_TIMEOUT}" \
-	"${@}"
+/usr/local/lib/wait-for-services.sh
+
+exec "$@"
