127effda697ad285fc4f8b01da9a4a8d522368ea
Jeffrey Böhm <hello@jeffrey-boehm.de>
2026-08-11 15:20:52 +0200

    chore(ci): solve several permission issues, improve job's reliability

--- .github/bin/popeye_score.sh
+++ .github/bin/popeye_score.sh
@@ -5,6 +5,9 @@ POPEYE_SCORE_THRESHOLD=76
 CURRENT_SCORE=$(popeye -o score)
 
 if [ "${CURRENT_SCORE}" -lt "${POPEYE_SCORE_THRESHOLD}" ]; then
+	# Only scan a second time for the human readable report when it is needed.
+	popeye || true
+
 	echo "Error: Popeye score is below ${POPEYE_SCORE_THRESHOLD}, current score: ${CURRENT_SCORE}"
 
 	exit 1
